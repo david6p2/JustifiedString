42727095252149c0f425f2e29f2c5b1c04a8aa20
David Andres Cespedes <ingdavidcespedes@gmail.com>
2017-04-11 17:40:23 -0500

    Disney Interview Answer

--- JustifiedString.playground/Contents.swift
+++ JustifiedString.playground/Contents.swift
@@ -5,43 +5,50 @@ import UIKit
 // Problem:
 // Given a String of n characteres, return a string of k desired characters by inserting evenly spaces between words
 
-func justifiedString(text:String, desiredLenght:Int) -> String {
-	// Take the diference between the current number of character and the desired number of characters
-	let lenghtDiff = desiredLenght - text.characters.count;
+func justifyString(_ currentString:String, requiredLenght lenghtReq:Int) -> String {
 	
-	// If the desired lenght is shorter than the current lenght, return the current string as it is
-	if lenghtDiff <= 0 {
-		print ("returned")
-		return text
+	
+	// lenght of the current string
+	let currentLenght = currentString.characters.underestimatedCount
+	// lenght of current string - required string
+	let missingCharacters = lenghtReq - currentLenght
+	
+	// if is current lenght is less that the requiere lenght, return the current string
+	if( missingCharacters <= 0){
+		return currentString
 	}
 	
-	// Separe the string by words in an Array
-	let words = text.components(separatedBy: " ")
+	// Divide the string by words
+	// ex: david and mike, will be 3 words, 2 places to insert spaces
+	let words = currentString.components(separatedBy: " ")
 	
-	//Divide the diference of characters between the current number of spaces of the string
-	let spaces = lenghtDiff / (words.count-1)
+	// Count the number of words, and divide the numbers of missing Characters in the number of words - 1 (missingCharacters / words - 1)
+	let numberOfSpaces = missingCharacters/(words.count - 1)
 	
-	//Create a new string with the calculated number of spaces, having in mind that you have to add the space that the current string has between words
-	let spacesStr = String(repeating:" ", count:spaces+1)
+	let spaces = String(repeating: " ", count: numberOfSpaces+1)
 	
-	var finalStr = ""
+	// Calculate if the calculated missingCharacters are exaclty the number of spaces to be inserted
+	var diferenceWithRequired = missingCharacters - (numberOfSpaces * (words.count - 1))
+	
+	//Concatenate String with new number of spaces
+	var concatenatedString = ""
 	
 	for i in 0..<(words.count-1) {
-		print(words[i])
-		//Concatenate each word with the calculated spaces for it
-		finalStr += words[i] + spacesStr
-		//If the difference between current characters and desired charactes is odd, you have to add an additional space.
-		//Let's do it after the first word
-		if (lenghtDiff%2 != 0 && i == 0) {
-			finalStr += " "
+		concatenatedString += words[i] + spaces
+		//If there will be missing charactes, insert them evenly from left to right until there are not characters left
+		if (diferenceWithRequired > 0){
+			concatenatedString += " "
+			diferenceWithRequired -= 1
 		}
+		
 	}
 	//Don't forget to add the last word
-	finalStr += words[words.count-1]
+	concatenatedString += words[words.count-1]
 	
-	return finalStr
+	return concatenatedString
+	
 }
 
-var stringName = justifiedString(text: "pepa perez cruz", desiredLenght: 22)
+var stringName = justifyString("pepa perez cruz tas", requiredLenght: 30)
 print(stringName)
 print(stringName.characters.count)
